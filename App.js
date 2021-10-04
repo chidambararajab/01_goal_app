87724681d60a73d57a77e3a34d61545c4a0d1d7b
chidambararajab <chidambararaja97@gmail.com>
2021-10-04 13:10:26 +0530

    Commit FlstList scrolling

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [enterGoal, setEnterGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnterGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { uid: Math.random().toString(), value: enterGoal },
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <View>
+        <FlatList
+          keyExtractor={(item, index) => item.uid}
+          data={courseGoals}
+          renderItem={(itemData) => (
+            <View style={styles.listItems}>
+              <Text>{itemData.item.value}</Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: { padding: 50 },
+
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  input: { width: "80%", backgroundColor: "orange", padding: 10 },
+
+  listItems: {
+    padding: 10,
+    marginTop: 10,
+    borderColor: "#070707",
+    borderWidth: 1,
+    backgroundColor: "#f7f7f7",
   },
 });
---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Notes
+
+## _poject notes_
+
+#
+
+#
+
+#### <FlatList />
+
+- The <FlatList /> component will have the lazy scrolling feqature, it only render the files when its scrolled on.
+  ` <FlatList data={courseGoals} renderItem={(itemData) => ( <View style={styles.listItems}> <Text>{itemData.item}</Text> </View> )} />`
+
+#
+
+#
+
+#### Heading
+
+- Points
+
+`Code`
